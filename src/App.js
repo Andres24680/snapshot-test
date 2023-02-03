d73799b973b1aa71df585c9ab85e8cae8ef7948a
Andres24680 <tasboyandresrestrepo@aol.com>
2023-02-02 21:36:33 -0500

    creating folders and test files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import GitHubCard from './GitHUbCard/GitHUbCard'
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <col style={{display: 'flex', justifyContent: 'center'}}>
+          <GitHubCard />
+        </col>
+      </Row>
     </div>
-  );
+  )
 }
-
 export default App;
